777aa955058c5e6ffdbc3f55da5a93555c07c15e
Ayushi Garg <34033088+AyushiGarg13@users.noreply.github.com>
2023-01-09 17:10:04 +0530

    Add model changes and migration, make sure migrations run before app starts.

--- migrations/env.py
+++ migrations/env.py
@@ -60,10 +60,7 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, 
-        target_metadata=get_metadata(),
-        compare_type=True,
-        literal_binds=True
+        url=url, target_metadata=get_metadata(), literal_binds=True
     )
 
     with context.begin_transaction():
@@ -95,7 +92,6 @@ def run_migrations_online():
             connection=connection,
             target_metadata=get_metadata(),
             process_revision_directives=process_revision_directives,
-            compare_type=True,
             **current_app.extensions['migrate'].configure_args
         )
 
